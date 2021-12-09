52f8a94b930ea2b48359ff5033ab8d4909f86b52
Jerrell Prier <jerrellprier@gmail.com>
2021-12-09 12:36:22 -0600

    Adding Java ArrayList demo

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        BufferedReader bufferedReader = null;
+        String filename = "/Users/jerre/Desktop/TweetData.txt";
+        String line = "";
+        List<String> hashtags = new ArrayList<String>();
+
+        try {
+            bufferedReader = new BufferedReader((new FileReader(filename)));
+            while((line = bufferedReader.readLine()) != null) {
+                //System.out.println("tweet = " + line);
+                String[] words = line.split(" ");
+                for(String word : words){
+                    if(word.contains("#")) {
+                        word = word.replaceAll("^[^#]*", "");
+                        if(!hashtags.contains(word))
+                            hashtags.add(word);
+                    }
+                }
+            }
+            hashtags.sort(String::compareTo);
+            for(String hashtag : hashtags){
+                System.out.println(hashtag);
+            }
+            System.out.println("Count = " + hashtags.size());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                bufferedReader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
